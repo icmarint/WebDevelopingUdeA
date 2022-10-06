6f02e42376592004c6d4a2056b0b551943b149c0
Isabel Marín <icmarint@unal.edu.co>
2022-10-06 08:00:22 -0500

    clase-03

--- 01-Primer-proyecto/holamundo.js
+++ 01-Primer-proyecto/holamundo.js
@@ -1,5 +1,91 @@
-alert("hola mundo");
+/* alert("hola mundo");
 
 var variable_1 = "Hola mundo";
 
 alert(variable_1);
+
+var variable_2 = 0;
+
+alert(variable_1);
+alert(variable_2);
+
+variable_2 = variable_1 + variable_2;
+
+alert(variable_1);
+
+variable_2 = variable_2 + 10;
+
+alert(variable_2);
+
+variable_2 = variable_2 + "10";
+
+alert(variable_2);
+
+console.log(variable_2);
+
+var arepas = ["Arepa rellena", "Arepa con queso", "Arepa sencilla"];
+
+console.log(arepas[1]);
+
+arepas[1] = "Arepa con carne";
+
+console.log(arepas);
+
+arepas.push("Arepa");
+
+console.log(arepas); */
+
+
+if (1 != 1){
+    console.log("esto es verdadero")
+}else{
+    console.log("esto es falso")
+}
+
+if (1 != 1){
+    console.log("esto es verdadero")
+}else if(2==2){
+    console.log("esto es verdadero x 2")
+}
+
+
+//Salta a error por ser estricto el tipo de dato con ===
+if (1 != 1){
+    console.log("esto es verdadero")
+}else if(2==="2"){
+    console.log("esto es verdadero x 2")
+}else{
+    console.log("error")
+}
+
+//Funciona por ser estricto el tipo de dato con ===
+if (1 != 1){
+    console.log("esto es verdadero")
+}else if(2===2){
+    console.log("esto es verdadero x 2")
+}else{
+    console.log("error")
+}
+
+
+//Ciclo for
+let areparellena = 5000;
+let arepaQueso = 10000;
+let arepaCarne = 15000;
+
+let bolsillo = 12000;
+
+valoresArepas = [areparellena, arepaQueso, arepaCarne];
+
+for(let i = 0; i<valoresArepas.length; i++){
+    console.log(valoresArepas[i])
+    if(bolsillo >= valoresArepas[i]){
+        console.log("Puedo comprar una arepa de valor "+valoresArepas[i])
+    }
+}
+
+//Dato curioso:
+const resultado = 9 + '1'
+const resultado2 = 9 - '1'
+console.log(resultado)
+console.log(resultado2)
